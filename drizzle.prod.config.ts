a3d7ae1a71603ed924f6e1e6c1965826c6430d65
Fugui <lacklock@gmail.com>
2026-08-12 11:32:46 +0800

    Update drizzle.prod.config.ts

--- drizzle.prod.config.ts
+++ drizzle.prod.config.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
-config({ path: ".env.prod" });
+config({ path: ".env.production" });
 
 export default defineConfig({
   out: "./drizzle",
